8803f3cfcaed2bf435d32d5aebaddc4b9a685c95
jamesanderson <jamesanderson@10.235.87.163>
2016-08-11 19:00:34 -0700

    Created unit tests that are separate from the code base. The unit tests check a function in exteranlFunction.py.

---
+++ SampleCode/UnitTest/externalFunction.py
@@ -0,0 +1,8 @@
+'''
+An example library file containing external functions to the test library.
+This is used to illustrate how to connect a test suite to a library file
+(i.e. keep them separate).
+'''
+ 
+def multiply(a, b):
+    return a * b
